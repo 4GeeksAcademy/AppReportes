d2e4018082521c68be62aa9347cb132a958f828a
Martín <martinjcaro@gmail.com>
2025-05-09 19:30:09 +0000

    user, reporte y media añadido a base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -14,6 +14,8 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    reportes: Mapped[List["Reporte"]] = relationship("Reporte", back_populates="user")
+
     def serialize(self):
         return {
             "id": self.id,
@@ -25,11 +27,59 @@ class Reporte(db.Model):
     __tablename__ = "reportes"
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    author_id = relationship('Favorites', back_populates='users')
+    author_id = relationship('User', back_populates='reportes')
 
     def serialize(self):
         return {
             "id": self.id,
-            "username": self.text,
-            "email": self.author_id
+            "text": self.text,
+            "author": self.author_id
         }
+    
+class Media(db.Model):
+    __tablename__ = "medias"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[enumerate] = mapped_column(String(120), unique=True, nullable=False)
+    image: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    post_id = relationship('Media', back_populates='reportes')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "image": self.image,
+            "post_id": self.post_id
+        }
+    
+# class Favorites(db.Model):
+#     __tablename__ = 'favorites'
+    
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    
+#     user: Mapped["User"] = relationship("User", back_populates="favorites")
+#     reporte: Mapped[Optional["Reporte"]] = relationship("Reporte", back_populates="favorites")  
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "user_id": self.user_id,
+#             "person_id": self.reporte
+#         }
+    
+# class Comments(db.Model):
+#     __tablename__ = 'comments'
+    
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     comment_text: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+#     votos: Mapped[bool] = mapped_column(unique=True, nullable=False)
+#     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+#     post_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "user_id": self.user_id,
+#             "person_id": self.reporte
+#         }
