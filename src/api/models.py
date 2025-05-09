767c8f78407af5c5a1757d44f469f49a778c60eb
Martín <martinjcaro@gmail.com>
2025-05-09 19:15:31 +0000

    preguardado database User

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from datetime import datetime
+from typing import List, Optional
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
+    
+class Reporte(db.Model):
+    __tablename__ = "reportes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    author_id = relationship('Favorites', back_populates='users')
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.text,
+            "email": self.author_id
         }
